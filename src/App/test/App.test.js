a2d45bc8331a1a18894b13b503be24d9e0e947e3
Jan Pozivil <crs1138@me.com>
2021-01-31 10:06:53 +0100

    🔩 Add Jest testing suite
    Details:
    ===
    
    * add Jest testing suite with support of testing ReacJS components

---
+++ src/App/test/App.test.js
@@ -0,0 +1,8 @@
+import { describe, expect, it } from '@jest/globals';
+import { sum } from '../App';
+
+describe('Testing the App', () => {
+    it('should be true', () => {
+        expect(1).toBe(1);
+    });
+});
